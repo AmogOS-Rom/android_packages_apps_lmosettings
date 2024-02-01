d7316aea1d357bb3ee8655899265f8ed4a4dd2a2
Davigamer987 <davimarchesiprimo@gmail.com>
2024-02-01 19:39:33 -0300

    Settings: Add Among US In Real Life Easter egg to Maintainer section
    Among us in real life, SUS, SUS
    
    Change-Id: I4f1d12003fb5d5bd7554a0b9c5d2cebe3ea46cac

--- src/com/android/settings/deviceinfo/firmwareversion/amogOSMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/amogOSMaintainerPreferenceController.java
@@ -1,22 +1,8 @@
-/*
- * Copyright (C) 2019 The LineageOS Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.android.settings.deviceinfo.firmwareversion;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.SystemProperties;
 import android.text.TextUtils;
 
@@ -29,6 +15,7 @@ public class amogOSMaintainerPreferenceController extends BasePreferenceControll
 
     private static final String TAG = "amogOSMaintainerPreferenceController";
     private static final String ROM_PROPERTY = "ro.amogos.maintainer";
+    private static final String YOUTUBE_URL = "https://www.youtube.com/watch?v=Ns3YxbIhTRM";
 
     public amogOSMaintainerPreferenceController(Context context, String key) {
         super(context, key);
@@ -42,5 +29,27 @@ public class amogOSMaintainerPreferenceController extends BasePreferenceControll
         String rom = SystemProperties.get(ROM_PROPERTY,
                 this.mContext.getString(R.string.device_info_default));
         return rom;
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            openYouTubeVideo();
+            return true;
+        }
+        return super.handlePreferenceTreeClick(preference);
+    }
+
+    private void openYouTubeVideo() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(YOUTUBE_URL));
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setPackage("com.google.android.youtube"); // Specify the YouTube app
+        if (intent.resolveActivity(mContext.getPackageManager()) != null) {
+            mContext.startActivity(intent);
+        } else {
+            // YouTube app not installed, open in a browser or handle as needed
+            intent.setPackage(null); // Clear the package to allow the system to handle it
+            mContext.startActivity(intent);
+        }
     }
 }
